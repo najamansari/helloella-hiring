e28e40f298a61fcd49977abf77f48b4163024f6d
Najam Ahmed Ansari <ahmed.najamansari@gmail.com>
2025-09-19 19:36:10 +0500

    Fixing deprecation warning
    sqlalchemy.ext.declarative.declarative_base has been moved to sqlalchemy.orm,
    refactoring accordingly.
    
    Signed-off-by: Najam Ahmed Ansari <ahmed.najamansari@gmail.com>

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine, Column, String, Text
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
